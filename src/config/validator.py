c67e19a83c632cacfc8d6e1fdbcaa12fb14d4ea2
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-06-24 14:05:22 -0500

    implemented validator config

---
+++ src/config/validator.py
@@ -0,0 +1,18 @@
+from .base import BaseConfig
+
+ENV_VALIDATOR_INTERVAL = "VALIDATOR_INTERVAL"
+
+class ValidatorConfig(BaseConfig):
+    def __init__(self, env_path='.env', ignore_config_file=False):
+        super().__init__(env_path, ignore_config_file)
+
+    def get_validator_interval(self) -> int:
+        interval = self._get(ENV_VALIDATOR_INTERVAL, None)
+
+        if interval is None or interval == '':
+            interval = '10'
+
+        if not interval.isdigit():
+            raise ValueError(f"The environment variable '{ENV_VALIDATOR_INTERVAL}' should only contain digits.")
+        
+        return int(interval)
