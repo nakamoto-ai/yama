074a59c5024bc76e01e4d1b994eff436221de78f
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-06-24 14:24:39 -0500

    parse cli args and print the env vars for now

--- src/miner/main.py
+++ src/miner/main.py
@@ -1,2 +1,17 @@
+import argparse
+from config.miner import MinerConfig
+
 if __name__ == '__main__':
-    print("Hello, Miner!")
+    parser = argparse.ArgumentParser(description="yama miner")
+    parser.add_argument("--env", type=str, default=".env", help="config file path")
+    parser.add_argument('--ignore-env-file', action='store_true', help='If set, ignore .env file')
+    args = parser.parse_args()
+    
+    config = MinerConfig(ignore_config_file=False)
+
+    try:
+        print(f"KEY_NAME: {config.get_key_name()}")
+        print(f"TESTNET: {config.get_testnet()}")
+        print(f"MINER_URL: {config.get_miner_url()}")
+    except ValueError as e:
+        print(e)
--- src/validator/main.py
+++ src/validator/main.py
@@ -1,2 +1,18 @@
+import argparse
+from config.validator import ValidatorConfig
+
 if __name__ == '__main__':
-    print("Hello, Validator!")
+    parser = argparse.ArgumentParser(description="yama validator")
+    parser.add_argument("--env", type=str, default=".env", help="config file path")
+    parser.add_argument('--ignore-env-file', action='store_true', help='If set, ignore .env file')
+    args = parser.parse_args()
+
+    config = ValidatorConfig(env_path=args.env, ignore_config_file=args.ignore_env_file)
+
+    try:
+        print(f"KEY_NAME: {config.get_key_name()}")
+        print(f"TESTNET: {config.get_testnet()}")
+        print(f"VALIDATOR_INTERVAL: {config.get_validator_interval()}")
+    except ValueError as e:
+        print(e)
+
