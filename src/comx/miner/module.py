b9b64dd865addf7ee0cc40f58df1fde636f5d57d
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-06-25 14:38:08 -0500

    implemented a scored miner module

--- src/comx/miner/module.py
+++ src/comx/miner/module.py
@@ -6,3 +6,11 @@ class MinerModule:
 
     def __repr__(self):
         return f"MinerModule(UID={self.uid}, SS58={self.ss58}, Address={self.address})"
+
+class ScoredMinerModule(MinerModule):
+    def __init__(self, uid: int, ss58: str, address: str, score: int):
+        super().__init__(uid=uid, ss58=ss58, address=address)
+        self.score = score
+
+    def __repr__(self):
+        return f"ScoredMinerModule(UID={self.uid}, SS58={self.ss58}, Address={self.address}, Score={self.score})"
