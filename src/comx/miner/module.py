46ae4bb59cb278cc1ffa25f9336123e41fe3f2e3
Eddie Nakamoto <edwardnakamoto@icloud.com>
2024-06-25 15:35:21 -0500

    add to_dict methods for json serializing

--- src/comx/miner/module.py
+++ src/comx/miner/module.py
@@ -4,6 +4,13 @@ class MinerModule:
         self.ss58 = ss58
         self.address = address
 
+    def to_dict(self) -> dict:
+        return {
+            "uid": self.uid,
+            "ss58": self.ss58,
+            "address": self.address
+        }
+
     def __repr__(self):
         return f"MinerModule(UID={self.uid}, SS58={self.ss58}, Address={self.address})"
 
@@ -11,6 +18,11 @@ class ScoredMinerModule(MinerModule):
     def __init__(self, uid: int, ss58: str, address: str, score: int):
         super().__init__(uid=uid, ss58=ss58, address=address)
         self.score = score
+
+    def to_dict(self) -> dict:
+        base_dict = super().to_dict()
+        base_dict["score"] = self.score
+        return base_dict
 
     def __repr__(self):
         return f"ScoredMinerModule(UID={self.uid}, SS58={self.ss58}, Address={self.address}, Score={self.score})"
