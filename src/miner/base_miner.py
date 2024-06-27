0eab6676aec0ef745076d105f5295d6d1f5827ef
Miller Wesselhoff <miller@nakamotomining.ai>
2024-06-27 16:27:25 -0500

    typing fix, parse_args -> pass typo fixed

--- src/miner/base_miner.py
+++ src/miner/base_miner.py
@@ -1,22 +1,11 @@
 import time
 from communex.module import Module, endpoint
-from keylimiter import TokenBucketLimiter
-from communex.module.server import ModuleServer
-import uvicorn
-from communex.compat.key import classic_load_key
 from loguru import logger
-from urllib.parse import urlparse
 from abc import abstractmethod
 
-from communex.client import CommuneClient
-
-from substrateinterface import Keypair
+from typing import Dict, Any
 
 class BaseMiner(Module):
-    def __init__(self, key: Keypair, client: CommuneClient, url: str):
-        self.key = key 
-        self.client = client 
-        self.url = url
 
     @endpoint
     def generate(self, prompt: str) -> Dict[str, Any]:
@@ -37,24 +26,4 @@ class BaseMiner(Module):
 
     @abstractmethod
     def generate_response(self, prompt: str):
-        parse_args
-    
-    @staticmethod
-    def start_miner_server(miner):
-        key = classic_load_key(miner.config.get_value("keyfile"))
-        url = miner.config.get_value("url")
-        parsed_url = urlparse(url)
-
-        refill_rate = 1 / 100
-
-        use_testnet = True if miner.config.get_value("isTestnet") == 1 else False
-        if use_testnet:
-            logger.info("Connecting to TEST network ...")
-        else:
-            logger.info("Connecting to main network ...")
-
-        bucket = TokenBucketLimiter(1000, refill_rate)
-        server = ModuleServer(miner, key, limiter=bucket, subnets_whitelist=[1], use_testnet=use_testnet)
-        app = server.get_fastapi_app()
-
-        uvicorn.run(app, host=parsed_url.hostname, port=parsed_url.port)
+        pass
