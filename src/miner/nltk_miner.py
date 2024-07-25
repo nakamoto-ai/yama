45bcea938d507954aa83ef4b3b18d192d67e65af
Miller Wesselhoff <miller@nakamotomining.ai>
2024-07-25 15:04:31 -0500

    move dataclass definition to resume_dataclasses.py

--- src/miner/nltk_miner.py
+++ src/miner/nltk_miner.py
@@ -14,30 +14,7 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from datasets import load_dataset
 from miner.base_miner import BaseMiner
-
-@dataclass
-class JobExperience:
-    title: str
-    company_name: str
-    description: str
-    start_date: str
-    end_date: str
-
-@dataclass
-class Education:
-    school: str
-    major: str
-    degree: str
-    start_date: str
-    end_date: str
-
-@dataclass
-class Resume:
-    skills: List[str] = field(default_factory=list)
-    work_experience: List[JobExperience] = field(default_factory=list)
-    education: List[Education] = field(default_factory=list)
-    certifications: List[str] = field(default_factory=list)
-    projects: List[str] = field(default_factory=list)
+from miner.resume_dataclasses import Resume, JobExperience, Education
 
 class DataLoader:
     """
